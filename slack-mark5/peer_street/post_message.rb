773ee1bb73f75ecede4e1b0cec513c6df128fbce
cpinky <chasepinky@gmail.com>
2017-08-17 09:48:56 -0700

    Fixed Changes

--- slack-mark5/peer_street/post_message.rb
+++ slack-mark5/peer_street/post_message.rb
@@ -9,18 +9,11 @@ module SlackMathbot
         require 'rubygems'
         require 'confluence/api/client'
 
-<<<<<<< HEAD
         username = 'cpinkerton@peerstreet.com'
         space    = 'General'
         url      = 'https://peerstreet.atlassian.net/wiki'
-=======
-        username = '...'
-        password = '...'
-        space    = '...'
-        url      = '...'
->>>>>>> 705d9b4054c43662af469f66e7e6f917e40842af
 
-        id = /https:\/\/domain.atlassian.net\/wiki\/spaces\/.*\/pages\/(\d+)\/.*/.match(event_hash["event"]["links"][0]["url"])[1]
+        id = /https:\/\/peerstreet.atlassian.net\/wiki\/spaces\/.*\/pages\/(\d+)\/.*/.match(event_hash["event"]["links"][0]["url"])[1]
 
 
         wiki = Confluence::Api::Client.new(username, password, url)
@@ -28,7 +21,6 @@ module SlackMathbot
         body = JSON.parse response.body
 
         client = Slack::Web::Client.new
-<<<<<<< HEAD
         client.chat_postMessage(channel: event_hash["event"]["channel"], "attachments": [
           {
             "fallback": "Required plain-text summary of the attachment.",
@@ -41,21 +33,6 @@ module SlackMathbot
             "text": Sanitize.clean(body["body"]["storage"]["value"]),
           }
         ])
-=======
-        client.chat_postMessage(channel: event_hash["event"]["channel"], text: 'Get unfurled', as_user: true, "attachments": [
-        {
-          "fallback": "Required plain-text summary of the attachment.",
-          "color": "#66CC33",
-          "author_name": "Wiki",
-          "author_link": "https://www.domain.com/",
-          "author_icon": "https://cwiki.apache.org/confluence/images/logo/default-space-logo-256.png",
-          "title": body["title"],
-          "text": body[""],
-        }
-    ]
-)
-
->>>>>>> 705d9b4054c43662af469f66e7e6f917e40842af
       end
     end
   end
